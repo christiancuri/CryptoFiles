d8f4b5bb5b907f19fcf5a24c6f8c0fae412b73a0
Christian Cury <contato.mikesantos@gmail.com>
2019-10-22 18:32:08 -0300

    chore

--- views/js/app.js
+++ views/js/app.js
@@ -1,13 +1,13 @@
 //webkitURL is deprecated but nevertheless
 URL = window.URL || window.webkitURL;
 
-var gumStream; 						//stream from getUserMedia()
-var recorder; 						//WebAudioRecorder object
-var input; 							//MediaStreamAudioSourceNode  we'll be recording
-var encodingType; 					//holds selected encoding for resulting audio (file)
-var encodeAfterRecord = true;       // when to encode
+var gumStream; //stream from getUserMedia()
+var recorder; //WebAudioRecorder object
+var input; //MediaStreamAudioSourceNode  we'll be recording
+var encodingType; //holds selected encoding for resulting audio (file)
+var encodeAfterRecord = true; // when to encode
 
-// shim for AudioContext when it's not avb. 
+// shim for AudioContext when it's not avb.
 var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext; //new audio context to help us record
 
@@ -20,310 +20,374 @@ recordButton.addEventListener("click", startRecording);
 stopButton.addEventListener("click", stopRecording);
 
 function startRecording() {
-	console.log("startRecording() called");
+  console.log("startRecording() called");
 
-	/*
+  /*
 		Simple constraints object, for more advanced features see
 		https://addpipe.com/blog/audio-constraints-getusermedia/
 	*/
-    
-    var constraints = { audio: true, video:false }
 
-    /*
+  var constraints = { audio: true, video: false };
+
+  /*
     	We're using the standard promise based getUserMedia() 
     	https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
 	*/
 
-	navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
-		__log("getUserMedia() success, stream created, initializing WebAudioRecorder...");
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(function(stream) {
+      __log(
+        "getUserMedia() success, stream created, initializing WebAudioRecorder..."
+      );
 
-		/*
+      /*
 			create an audio context after getUserMedia is called
 			sampleRate might change after getUserMedia is called, like it does on macOS when recording through AirPods
 			the sampleRate defaults to the one set in your OS for your playback device
 
 		*/
-		audioContext = new AudioContext();
+      audioContext = new AudioContext();
 
-		//update the format 
-		document.getElementById("formats").innerHTML="Format: 2 channel "+encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value+" @ "+audioContext.sampleRate/1000+"kHz"
+      //update the format
+      document.getElementById("formats").innerHTML =
+        "Format: 2 channel " +
+        encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value +
+        " @ " +
+        audioContext.sampleRate / 1000 +
+        "kHz";
 
-		//assign to gumStream for later use
-		gumStream = stream;
-		
-		/* use the stream */
-		input = audioContext.createMediaStreamSource(stream);
-		
-		//stop the input from playing back through the speakers
-		//input.connect(audioContext.destination)
+      //assign to gumStream for later use
+      gumStream = stream;
 
-		//get the encoding 
-		encodingType = encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
-		
-		//disable the encoding selector
-		encodingTypeSelect.disabled = true;
+      /* use the stream */
+      input = audioContext.createMediaStreamSource(stream);
 
-		recorder = new WebAudioRecorder(input, {
-		  workerDir: "js/", // must end with slash
-		  encoding: encodingType,
-		  numChannels:2, //2 is the default, mp3 encoding supports only 2
-		  onEncoderLoading: function(recorder, encoding) {
-		    // show "loading encoder..." display
-		    __log("Loading "+encoding+" encoder...");
-		  },
-		  onEncoderLoaded: function(recorder, encoding) {
-		    // hide "loading encoder..." display
-		    __log(encoding+" encoder loaded");
-		  }
-		});
+      //stop the input from playing back through the speakers
+      //input.connect(audioContext.destination)
 
-		recorder.onComplete = async function(recorder, blob) { 
-      __log("Encoding complete");
-      
-      await try02(recorder, blob)
+      //get the encoding
+      encodingType =
+        encodingTypeSelect.options[encodingTypeSelect.selectedIndex].value;
 
-			encodingTypeSelect.disabled = false;
-		}
+      //disable the encoding selector
+      encodingTypeSelect.disabled = true;
 
-		recorder.setOptions({
-		  timeLimit:120,
-		  encodeAfterRecord:encodeAfterRecord,
-	      ogg: {quality: 0.5},
-	      mp3: {bitRate: 160}
-	    });
+      recorder = new WebAudioRecorder(input, {
+        workerDir: "js/", // must end with slash
+        encoding: encodingType,
+        numChannels: 2, //2 is the default, mp3 encoding supports only 2
+        onEncoderLoading: function(recorder, encoding) {
+          // show "loading encoder..." display
+          __log("Loading " + encoding + " encoder...");
+        },
+        onEncoderLoaded: function(recorder, encoding) {
+          // hide "loading encoder..." display
+          __log(encoding + " encoder loaded");
+        }
+      });
 
-		//start the recording process
-		recorder.startRecording();
+      recorder.onComplete = async function(recorder, blob) {
+        __log("Encoding complete");
 
-		 __log("Recording started");
+        await try02(recorder, blob);
 
-	}).catch(function(err) {
-	  	//enable the record button if getUSerMedia() fails
-    	recordButton.disabled = false;
-    	stopButton.disabled = true;
+        encodingTypeSelect.disabled = false;
+      };
 
-	});
+      recorder.setOptions({
+        timeLimit: 120,
+        encodeAfterRecord: encodeAfterRecord,
+        ogg: { quality: 0.5 },
+        mp3: { bitRate: 160 }
+      });
 
-	//disable the record button
-    recordButton.disabled = true;
-    stopButton.disabled = false;
+      //start the recording process
+      recorder.startRecording();
+
+      __log("Recording started");
+    })
+    .catch(function(err) {
+      //enable the record button if getUSerMedia() fails
+      recordButton.disabled = false;
+      stopButton.disabled = true;
+    });
+
+  //disable the record button
+  recordButton.disabled = true;
+  stopButton.disabled = false;
+}
+
+const playButton = document.getElementById("start");
+const audioId = document.getElementById("audioId");
+
+playButton.addEventListener("click", forcePlayAudio);
+
+async function forcePlayAudio() {
+  const id = audioId.value;
+  if (id == "") {
+    return alert("Type audio id");
+  }
+  axios
+    .get(`/audio/${id}`)
+    .then(data => data.data)
+    .then(data => data.data)
+    .then(async data => {
+      const decrypted = await decrypt(data);
+      const audio = new Audio(URL.createObjectURL(decrypted));
+      audio.play();
+    });
+}
+
+// axios
+//   .get("/audio/sample")
+//   .then(data => data.data)
+//   .then(data => data.data)
+//   .then(async data => {
+//     const decrypted = await decrypt(data);
+//     const audio = new Audio(URL.createObjectURL(decrypted));
+//     audio.play();
+//   });
+
+let lastAudio;
+const resendbutton = document.getElementById("reSend");
+resendbutton.addEventListener("click", resendFile);
+async function resendFile() {
+  const formData = new FormData();
+  const encryptedBlob = lastAudio;
+
+  const currentAudioId = new Date().getTime();
+
+  formData.append("data", encryptedBlob);
+  formData.append(`name`, currentAudioId);
+
+  axios.post("/upload", formData).then(dat => {
+    const data = dat.data;
+    const url = data.url;
+    axios
+      .get(url)
+      .then(data => data.data)
+      .then(data => data.data)
+      .then(async data => {
+        const decrypted = await decrypt(data);
+        const audio = new Audio(URL.createObjectURL(decrypted));
+        audio.play();
+      });
+  });
 }
 
 async function try02(recorder, blob) {
-  const formData = new FormData()
-  const encryptedBlob = await encrypt(blob)
-  console.log(encryptedBlob)
-  
-  formData.append('data', encryptedBlob)
-  formData.append(`name`, new Date().getTime())
-  
-  axios.post('/upload', formData)
-    .then(dat => {
-      const data = dat.data
-      const url = data.url
-      axios.get(url)
-        .then(data => data.data)
-        .then(data => data.data)
-        .then(async data => {
-          const decrypted = await decrypt(data)
-          const audio = new Audio(URL.createObjectURL(decrypted))
-          audio.play()
-        })
-    })
+  const formData = new FormData();
+  const encryptedBlob = await encrypt(blob);
+
+  const currentAudioId = new Date().getTime();
+  const file = arrayBufferToFile(encryptedBlob, currentAudioId);
+
+  formData.append("data", file);
+  formData.append(`name`, currentAudioId);
+
+  console.log(currentAudioId);
+  lastAudio = encryptedBlob;
+
+  axios.post("/upload", formData).then(dat => {
+    const data = dat.data;
+    const url = data.url;
+    // console.log(data);
+    fetch(url)
+      .then(data => data.blob())
+      .then(async data => {
+        const decrypted = await decrypt(data);
+        const audio = new Audio(URL.createObjectURL(decrypted));
+        audio.play();
+      });
+    // axios
+    //   .get(url)
+    //   .then(res => res.blob())
+    //   // .then(data => data.data)
+    //   .then(async data => {
+    //     const decrypted = await decrypt(data);
+    //     const audio = new Audio(URL.createObjectURL(decrypted));
+    //     audio.play();
+    //   });
+  });
 }
 
-let keyless 
+function arrayBufferToFile(ab, fileName) {
+  return new File([ab], fileName);
+}
+
+let keyless;
 const iv = crypto.getRandomValues(new Uint8Array(16));
-generateKey()
+generateKey();
 async function generateKey() {
-  keyless = await crypto.subtle.generateKey({ 'name': 'AES-CBC', 'length': 256 }, false, ['encrypt', 'decrypt'])
+  keyless = await crypto.subtle.generateKey(
+    { name: "AES-CBC", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
+  );
+  console.log(keyless);
+  console.log("key generated");
 }
 
 async function encrypt(blob) {
-  return new Promise((resolve) => {
-    const reader = new FileReader() 
-    reader.readAsArrayBuffer(blob)
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(blob);
     reader.onload = () => {
-      const arrayBuffer = reader.result
+      const arrayBuffer = reader.result;
 
-      crypto.subtle.encrypt({ 'name': 'AES-CBC', iv }, keyless, arrayBuffer)
-        // .then(encrypted => resolve(btoa(String.fromCharCode(...new Uint8Array(encrypted)))))
+      crypto.subtle
+        .encrypt({ name: "AES-CBC", iv }, keyless, arrayBuffer)
         .then(encrypted => {
-          const b64 = Base64Binary.encode(encrypted)
-          // const a = btoa(unescape(String.fromCharCode(...new Int32Array(encrypted))))
-          // console.log(b64)
-          resolve(b64)
+          resolve(encrypted);
+          // console.log(encrypted);
+          // const b64 = Base64Binary.encode(encrypted);
+          // resolve(b64);
         })
-        .catch(console.error)
-    }
-  })
+        .catch(console.error);
+    };
+  });
 }
 
 var Base64Binary = {
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-	
-	/* will return a  Uint8Array type */
-	decodeArrayBuffer: function(input) {
-		var bytes = (input.length/4) * 3;
-		var ab = new ArrayBuffer(bytes);
-		this.decode(input, ab);
-		
-		return ab;
-	},
+  key: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
-	removePaddingChars: function(input){
-		var lkey = this._keyStr.indexOf(input.charAt(input.length - 1));
-		if(lkey == 64){
-			return input.substring(0,input.length - 1);
-		}
-		return input;
-	},
-
-	decode: function (input, arrayBuffer) {
-		//get last chars to see if are valid
-		input = this.removePaddingChars(input);
-		input = this.removePaddingChars(input);
-
-		var bytes = parseInt((input.length / 4) * 3, 10);
-		
-		var uarray;
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
-		var j = 0;
-		
-		if (arrayBuffer)
-			uarray = new Uint8Array(arrayBuffer);
-		else
-			uarray = new Uint8Array(bytes);
-		
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-		
-		for (i=0; i<bytes; i+=3) {	
-			//get the 3 octects in 4 ascii chars
-			enc1 = this._keyStr.indexOf(input.charAt(j++));
-			enc2 = this._keyStr.indexOf(input.charAt(j++));
-			enc3 = this._keyStr.indexOf(input.charAt(j++));
-			enc4 = this._keyStr.indexOf(input.charAt(j++));
-	
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-	
-			uarray[i] = chr1;			
-			if (enc3 != 64) uarray[i+1] = chr2;
-			if (enc4 != 64) uarray[i+2] = chr3;
-		}
-	
-		return uarray;	
+  decodeArrayBuffer: function(input) {
+    const bytes = (input.length / 4) * 3;
+    const arrayBuffer = new ArrayBuffer(bytes);
+    this.decode(input, arrayBuffer);
+    return arrayBuffer;
   },
-  
-  encode: arrayBuffer => {
-    var base64    = ''
-    var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 
-    var bytes         = new Uint8Array(arrayBuffer)
-    var byteLength    = bytes.byteLength
-    var byteRemainder = byteLength % 3
-    var mainLength    = byteLength - byteRemainder
+  sanitize: function(input) {
+    const lkey = this.key.indexOf(input.charAt(input.length - 1));
+    return lkey == 64 ? input.substring(0, input.length - 1) : input;
+  },
 
-    var a, b, c, d
-    var chunk
+  decode: function(input, arrayBuffer) {
+    input = this.sanitize(this.sanitize(input));
 
-    // Main loop deals with bytes in chunks of 3
+    const bytes = parseInt((input.length / 4) * 3, 10);
+
+    let i,
+      j = 0;
+
+    const byteArray = new Uint8Array(arrayBuffer ? arrayBuffer : bytes);
+
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+    for (i = 0; i < bytes; i += 3) {
+      const enc1 = this.key.indexOf(input.charAt(j++));
+      const enc2 = this.key.indexOf(input.charAt(j++));
+      const enc3 = this.key.indexOf(input.charAt(j++));
+      const enc4 = this.key.indexOf(input.charAt(j++));
+
+      const char1 = (enc1 << 2) | (enc2 >> 4);
+      const char2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+      const char3 = ((enc3 & 3) << 6) | enc4;
+
+      byteArray[i] = char1;
+      if (enc3 != 64) byteArray[i + 1] = char2;
+      if (enc4 != 64) byteArray[i + 2] = char3;
+    }
+
+    return byteArray;
+  },
+
+  encode: function(arrayBuffer) {
+    let base64 = "";
+
+    const bytes = new Uint8Array(arrayBuffer);
+    const byteLength = bytes.byteLength;
+    const byteRemainder = byteLength % 3;
+    const mainLength = byteLength - byteRemainder;
+
+    let a, b, c, d;
+    let chunk;
+
     for (var i = 0; i < mainLength; i = i + 3) {
-      // Combine the three bytes into a single integer
-      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+      chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
 
-      // Use bitmasks to extract 6-bit segments from the triplet
-      a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
-      b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
-      c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
-      d = chunk & 63               // 63       = 2^6 - 1
+      a = (chunk & 16515072) >> 18;
+      b = (chunk & 258048) >> 12;
+      c = (chunk & 4032) >> 6;
+      d = chunk & 63;
 
-      // Convert the raw binary segments to the appropriate ASCII encoding
-      base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+      base64 += this.key[a] + this.key[b] + this.key[c] + this.key[d];
     }
 
-    // Deal with the remaining bytes and padding
     if (byteRemainder == 1) {
-      chunk = bytes[mainLength]
-
-      a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
-
-      // Set the 4 least significant bits to zero
-      b = (chunk & 3)   << 4 // 3   = 2^2 - 1
-
-      base64 += encodings[a] + encodings[b] + '=='
+      chunk = bytes[mainLength];
+      a = (chunk & 252) >> 2;
+      b = (chunk & 3) << 4;
+      base64 += this.key[a] + this.key[b] + "==";
     } else if (byteRemainder == 2) {
-      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
-
-      a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
-      b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
-
-      // Set the 2 least significant bits to zero
-      c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
-
-      base64 += encodings[a] + encodings[b] + encodings[c] + '='
+      chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+      a = (chunk & 64512) >> 10;
+      b = (chunk & 1008) >> 4;
+      c = (chunk & 15) << 2;
+      base64 += this.key[a] + this.key[b] + this.key[c] + "=";
     }
-    
-    return base64
+    return base64;
   }
-}
+};
 
 async function decrypt(encryptedBlob) {
-  return new Promise((resolve) => {
-    // const arrayBuffer = Int32Array.from(atob(encryptedBlob), c => c.charCodeAt(0))
-    const arrayBuffer = Base64Binary.decode(encryptedBlob)
-
-    crypto.subtle.decrypt({ 'name': 'AES-CBC', iv }, keyless, arrayBuffer)
-      .then(decrypted => resolve(new Blob([decrypted], {type: 'audio/ogg'})))
-  })
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const r = e.target.result;
+      crypto.subtle
+        .decrypt({ name: "AES-CBC", iv }, keyless, r)
+        .then(decrypted => {
+          resolve(new Blob([decrypted], { type: "audio/ogg" }));
+        })
+        .catch(console.error);
+    };
+    reader.readAsArrayBuffer(encryptedBlob);
+  });
 }
 
 function stopRecording() {
-	console.log("stopRecording() called");
-	
-	//stop microphone access
-	gumStream.getAudioTracks()[0].stop();
+  console.log("stopRecording() called");
 
-	//disable the stop button
-	stopButton.disabled = true;
-	recordButton.disabled = false;
-	
-	//tell the recorder to finish the recording (stop recording + encode the recorded audio)
-	recorder.finishRecording();
+  //stop microphone access
+  gumStream.getAudioTracks()[0].stop();
 
-	__log('Recording stopped');
+  //disable the stop button
+  stopButton.disabled = true;
+  recordButton.disabled = false;
+
+  //tell the recorder to finish the recording (stop recording + encode the recorded audio)
+  recorder.finishRecording();
+
+  __log("Recording stopped");
 }
 
-function createDownloadLink(blob,encoding) {
-	
-	var url = URL.createObjectURL(blob);
-	var au = document.createElement('audio');
-	var li = document.createElement('li');
-	var link = document.createElement('a');
+function createDownloadLink(blob, encoding) {
+  var url = URL.createObjectURL(blob);
+  var au = document.createElement("audio");
+  var li = document.createElement("li");
+  var link = document.createElement("a");
 
-	//add controls to the <audio> element
-	au.controls = true;
-	au.src = url;
+  //add controls to the <audio> element
+  au.controls = true;
+  au.src = url;
 
-	//link the a element to the blob
-	link.href = url;
-	link.download = new Date().toISOString() + '.'+encoding;
-	link.innerHTML = link.download;
+  //link the a element to the blob
+  link.href = url;
+  link.download = new Date().toISOString() + "." + encoding;
+  link.innerHTML = link.download;
 
-	//add the new audio and a elements to the li element
-	li.appendChild(au);
-	li.appendChild(link);
+  //add the new audio and a elements to the li element
+  li.appendChild(au);
+  li.appendChild(link);
 
-	//add the li element to the ordered list
-	recordingsList.appendChild(li);
+  //add the li element to the ordered list
+  recordingsList.appendChild(li);
 }
-
-
 
 //helper function
 function __log(e, data) {
-	log.innerHTML += "\n" + e + " " + (data || '');
+  log.innerHTML += "\n" + e + " " + (data || "");
 }
